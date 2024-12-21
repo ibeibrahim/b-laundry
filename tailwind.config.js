11900b2de1cc2a6c64e17473d8515818e5aa3102
ibeaim <aimzahra001@gmail.com>
2024-12-22 00:54:09 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
@@ -11,11 +12,29 @@ module.exports = {
         poppins: ['Poppins', 'sans-serif'],
       },
       backgroundImage: {
-        'main': "url('/src/images/bg.jpeg')"
-      }
+        'main': "url('/src/images/bg.jpeg')",
+        'karyawan': "url('/src/images/bg-karyawan.jpeg')",
+      }, 
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+    require('daisyui')
+  ],
 }
 
 // 
